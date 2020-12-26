6274131ba62ea71bda7d609d00811a320ce9e2a3
MmeOokamy <manue.ggt@gmail.com>
2020-12-26 19:29:38 +0100

    interface for crud?

---
+++ src/Models/ModelInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+interface ModelInterface
+{
+    public function create(){}
+    public function read(){}
+    public function update(){}
+    public function delete(){}
+    
+
+}
